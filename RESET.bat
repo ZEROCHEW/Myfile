da47327f5c0b71158da756f232f57b6e2b2cd34c
kodomo <94239986+ZEROCHEW@users.noreply.github.com>
2025-02-01 23:18:41 +0700

    Update RESET.bat

--- RESET.bat
+++ RESET.bat
@@ -1,3 +1,8 @@
+@shift /0
+@echo off
+chcp 65001
+for /f "tokens=1,2 delims=#" %%a in ('"prompt #$h#$e# & echo on & for %%b in (1) do rem"') do (set "del=%%a" & set "col=%%b")
+call :IsAdmin
 netsh winsock reset
 netsh int ip reset
 netsh advfirewall reset
